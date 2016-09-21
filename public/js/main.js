c15314a524acdfe4b42df10a30aab98554dbc62f
Oleg Korolko <olegk101@gmail.com>
2016-09-21 10:56:58 +0300

    fixed fade out

--- public/js/main.js
+++ public/js/main.js
@@ -12,10 +12,32 @@ $(document).ready(function() {
   var headerLogo = document.querySelectorAll(".header-logo")[0];
   var slidingPartText = document.querySelectorAll(".mobile-menu")[0];
   var link = $(".mobile-menu__elem-link");
+  var item = $( '.mobile-modal' );
 
 
-  link.click(function() {
+  var bodyWrapper =  document.querySelectorAll(".main-container")[0];
+  var preloader = document.querySelectorAll(".preloader-container")[0];
+
+  if (bodyWrapper.classList.contains('not-active') === true) {
+    setTimeout(function() {
+      bodyWrapper.classList.remove('not-active')
+      preloader.classList.add('not-active')
+    }, 100)
+  }
+
+
+  link.click(function(e) {
+    e.preventDefault();
     menuEventsHandler(navIcon, modal, slidingPart, headerLogo, slidingPartText)
+    var self = this;
+
+    item.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
+      function() {
+        window.location = self.href;
+        bodyWrapper.classList.add('fade-out')
+      });
+
+
   })
 
   navIcon.addEventListener( "click", function(e) {
@@ -33,17 +55,6 @@ $(document).ready(function() {
       args.forEach(function(toggle) {
          (toggle.classList.contains('is-active') === true) ? toggle.classList.remove('is-active') : toggle.classList.add('is-active')
     })
-  }
-
-
-  var bodyWrapper =  document.querySelectorAll(".main-container")[0];
-  var preloader = document.querySelectorAll(".preloader-container")[0];
-
-  if (bodyWrapper.classList.contains('not-active') === true) {
-    setTimeout(function() {
-      bodyWrapper.classList.remove('not-active')
-      preloader.classList.add('not-active')
-    }, 100)
   }
 
 })();
