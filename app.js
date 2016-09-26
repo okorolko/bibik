2f42f6734eda8f4d75f0e80aa8a72b5c2636275b
Oleg Korolko <olegk101@gmail.com>
2016-09-26 19:49:13 +0300

    fixed wrong route to cap pic

--- app.js
+++ app.js
@@ -232,7 +232,6 @@ app.get('/api/instagram', apiController.getInstagram);
  * 404
  */
 app.use(function(req, res, next) {
-	// res.status(404).send('Sorry cant find that!');
 	res.status(404);
 	res.render('404', {
 		error: '404'
