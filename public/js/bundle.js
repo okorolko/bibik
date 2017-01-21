6ee027fc549a0463be9e95dd96444d8663f1174e
Oleg Korolko <okorolko@Olegs-MBP.Dlink>
2017-01-21 22:21:40 +0300

    deleted unused commented code

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,3 +1,6 @@
+// Lazy load
+var myLazyLoad = new LazyLoad();
+
 
 //Mobile/Desktop menu control
 	(function() {
@@ -66,25 +69,4 @@ window.onload = function() {
 
 
 
-var myLazyLoad = new LazyLoad();
 
-// $(window).scroll(function() {
-// 	var loaded = true;
-
-//    if (loaded && ($(window).scrollTop() + $(window).height() > $(document).height() - 100)) {
-//        console.log("near bottom!");
-
-// 	$.get( "/lookbooknext", function( data ) {
-// 		loaded = false;
-// 		data.forEach(function(src) {
-// 			// console.log(src)
-// 			$( ".lookbook-container" ).append( '<div class=".lookbook__elem"><img src=' + src + '></div>' );
-// 		})
-// 		loaded = true;
-// 	});
-
-//    }
-// });
-// .lookbook-container
-// .lookbook__elem
-//                 img(src=item)
